46eeb9a4721c9ff96f52511f4ce47f195608bc65
Isabel Fernandez abad <isfernan@c1r7s3.42madrid.com>
2022-11-17 20:31:32 +0100

    Case 4 of deleteNode left

--- utils/BSTNode.hpp
+++ utils/BSTNode.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   BSTree.hpp                                         :+:      :+:    :+:   */
+/*   BSTNode.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: isfernan <isfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:16:12 by isfernan          #+#    #+#             */
-/*   Updated: 2022/11/17 19:16:46 by isfernan         ###   ########.fr       */
+/*   Updated: 2022/11/17 20:22:34 by isfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,6 @@
 
 namespace ft{
 
-template <typename T>
-struct Node
-{
-	typedef T	value_type;
-
-	
-
-	Node(value_type const &src) : data(src) { }
-	Node(value_type const &src, Node *l, Node *r, Node *p) : data(src), left(l), right(r), parent(p) { }
-	Node(Node *l, Node *r, Node *p) : left(l), right(r), parent(p) { }
-};
-
 template <typename T, typename Compare, typename Alloc = std::allocator<Node<T> > >
 class BSTNode
 {
@@ -38,47 +26,124 @@ class BSTNode
 		typedef Compare				key_compare;
 		typedef Alloc				allocator_type;
 		typedef size_t				size_type;
-		typedef Node<value_type>	node;
-		typedef Node				*Node_ptr;
 
 		value_type	data;
-		node		*left;
-		node		*right;
-		node		*parent;
-	
-		// Constructors & Destructor
-		BSTNode(const key_compare& comp = key_compare(), 
-			const allocator_type& alloc = allocator_type()): _alloc(alloc), _comp(comp)
-		{
-			_size = 0;
-			_root = NULL;
-		}
-
-		BSTNode(BSTNode const& copy)
-		{
-			_alloc = copy._alloc;
-			_comp = copy._comp;
-			_size = copy._size;
-			copy_node(*this, copy.root);
-		}
+		BSTNode		*left;
+		BSTNode		*right;
+		BSTNode		*parent;
 
 		private:
 			key_compare				_comp;
 			allocator_type			_alloc;
+	
+		public:
+		// Constructors & Destructor
+		BSTNode(value_type d = value_type(), const key_compare& comp = key_compare(), 
+			const allocator_type& alloc = allocator_type()): _alloc(alloc), _comp(comp), data(d)
+		{
+			this->left = 0;
+			this->right = 0;
+			this->parent = 0;
+			return ;
+		}
+
+		// BSTNode(BSTNode const& copy)
+		// {
+		// 	_alloc = copy._alloc;
+		// 	_comp = copy._comp;
+		// 	_size = copy._size;
+		// 	copy_node(*this, copy.root);
+		// }
 
 		// operator=
-		BSTNode operator=(BSTNode const& copy)
+		BSTNode			operator=(BSTNode const& copy)
 		{
 			if (this != &copy)
 			{
-				_alloc = copy._alloc;
-				_comp = copy._comp;
-				_size = copy._size;
-				_deepCopy(*this, copy._root);
+				this->data = copy.data;
+				this->left = copy.left;
+				this->right = copy.right;
+				this->parent = copy.parent;
+				this->_comp = copy.key_comp();
+				this->_alloc = copy.get_allocator();
 			}
 			return *this;
 		}
+
+		// Getters
+
+		allocator_type	get_allocator(void) const { return (this->_alloc); 	}
+
+		key_compare		key_comp(void) const { return (this->_comp); }
+
+		// Insert
+		// HE CAMBIADO DATA POR D, REVISAR SI ESTA BIEN
+		BSTNode*&		insert(BSTNode *&root, value_type d)
+		{
+			if (!root)
+			{
+				root = this->_alloc.allocate(1);
+				this->_alloc.construct(root, BSTNode(d));
+				return (root);
+			}
+			else if (this->_comp(d.first, root->data.first))
+			{
+				root->left = insert(root->left, d);
+				root->left->parent = root;
+			}
+			else if (d.first != root->data.first)
+			{
+				root->right = insert(root->right, d);
+				root->right->parent = root;
+			}
+			return (root);
+		}
+
+		// Find
+		BSTNode*		search(BSTNode *root, value_type d) const
+		{
+			if (!root || root->data.first == d.first)
+				return (root);
+			else if (this->_comp(d.first, root->data.first))
+				search(root->left, d);
+			return (search(root->right, d));
+		}
 		
+		// Delete
+		BSTNode*		deleteNode(BSTNode *root, value_type data)
+		{
+			BSTNode	*temp;
+			// Case 0: no root
+			if (!root)
+				return (root);
+			// Case 1: leaf node
+			if (!root->right && !root->left)
+			{
+				this->_alloc.destroy(root);
+				this->_alloc.deallocate(root, 1);
+			}
+			// Case 2: 1 left child
+			if (!root->right)
+			{
+				temp = root->left;
+				temp->parent = root->parent;
+				this->_alloc.destroy(root);
+				this->_alloc.deallocate(root, 1);
+			}
+			// Case 3: 1 right child
+			if (!root->left)
+			{
+				temp = root->right;
+				temp->parent = root->parent;
+				this->_alloc.destroy(root);
+				this->_alloc.deallocate(root, 1);
+			}
+			// Case 4: 2 children
+			else
+			{
+				
+			}
+		}
 };
 
 
