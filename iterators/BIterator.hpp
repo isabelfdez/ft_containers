d26f1856319d6713720469fff5ed7a815368ae05
Isabel Fernandez abad <isfernan@c1r4s5.42madrid.com>
2022-11-22 19:35:13 +0100

    a bit of the map

--- iterators/BIterator.hpp
+++ iterators/BIterator.hpp
@@ -6,7 +6,7 @@
 /*   By: isfernan <isfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:14:30 by isfernan          #+#    #+#             */
-/*   Updated: 2022/11/18 18:02:12 by isfernan         ###   ########.fr       */
+/*   Updated: 2022/11/22 16:13:05 by isfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,35 +34,107 @@ class BIterator
 		typedef T&													reference;
         
     private:
-        node_ptr    _node;
-        node_ptr    _end;
-        node_ptr    _beginning;
-        
-    public:
-        BIterator(node_ptr node = 0) : _node(node)
-        {
-            node_ptr	tmp(node);
+        node_ptr	_node;
+        node_ptr	_end;
+        node_ptr	_beginning;
 
-			if (node && (tmp || tmp == this->_end))
+	public:
+		BIterator(node_ptr node = 0) : _node(node) { }
+
+		Iterate(node_ptr node)
+		{
+			node_ptr smallest = Leftmost(node);
+			while (node)
 			{
-				while (tmp->parent && tmp->parent->parent)
-					tmp = tmp->parent;
-				while (tmp->left)
-					tmp = tmp->left;
-				this->_beg = tmp;
-				tmp = node;
-				while (tmp->parent)
-					tmp = tmp->parent;
-				this->_end = tmp;
+				std::cout << node->data << std::endl;
+				node = Next(node);
 			}
-			else
-			{
-				this->_beg = 0;
-				this->_end = 0;
-			}
-			return ;
-        }
+		}
+
+	private:
+		node_ptr Leftmost(node_ptr node)
+		{
+		    if (node == nullptr)
+		        return nullptr;
+		    while (node->left != nullptr)
+		        node = node->left;
+		    return node;
+		}
+		
+		// Start iterating from a root node
+		node_ptr First(node_ptr root)
+		{
+		    return Leftmost(root);
+		}
+		
+		// The iteration is current at node.  Return the next node
+		// in value order.
+		node_ptr Next(node_ptr node)
+		{
+		    // Make sure that the caller hasn't failed to stop.
+		    assert(node != nullptr);
+		
+		    // If we have a right subtree we must iterate over it,
+		    // starting at its leftmost (minimal) node.
+		
+		    if (node->right != nullptr)
+		        return Leftmost(node->right);
+		
+		    // Otherwise we must go up the tree
+		
+		    node_ptr* parent = node->parent;
+		    if (parent == nullptr)
+		        return nullptr;
+		
+		    // A node comes immediately after its left subtree
+		
+		    if (node == parent->left)
+		        return parent;
+		
+		    // This must be the right subtree!
+		    assert(node == parent->right);
+		
+		    // In which case we need to go up again, looking for a node that is
+		    // its parent's left child.
+		
+		    while (parent != nullptr && node != parent->left)
+		    {
+		        node = parent;
+		        parent = node->parent;
+		    }
+		
+		    // We should be at a left child!
+		    assert(parent == nullptr || node == parent->left);
+		
+		    // And, as we know, a node comes immediately after its left subtree
+		
+		    return parent;
+		}
+		
     // Constructors & destructor
+        // BIterator(node_ptr node = 0) : _node(node)
+        // {
+        //     node_ptr	tmp(node);
+
+		// 	if (node && (tmp || tmp == this->_end))
+		// 	{
+		// 		while (tmp->parent && tmp->parent->parent)
+		// 			tmp = tmp->parent;
+		// 		while (tmp->left)
+		// 			tmp = tmp->left;
+		// 		this->_beg = tmp;
+		// 		tmp = node;
+		// 		while (tmp->parent)
+		// 			tmp = tmp->parent;
+		// 		this->_end = tmp;
+		// 	}
+		// 	else
+		// 	{
+		// 		this->_beg = 0;
+		// 		this->_end = 0;
+		// 	}
+		// 	return ;
+        // }
         
 };
 
--- map.hpp
+++ map.hpp
@@ -6,7 +6,7 @@
 /*   By: isfernan <isfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 12:55:18 by isfernan          #+#    #+#             */
-/*   Updated: 2022/11/18 17:32:00 by isfernan         ###   ########.fr       */
+/*   Updated: 2022/11/22 16:31:31 by isfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <iostream>
 # include "utils/pair.hpp"
 # include "utils/BSTNode.hpp"
+# include "iterators/BIterator.hpp"
 
 namespace ft {
 
@@ -25,16 +26,87 @@ class map
 {
 	public:
 	// Redefinition of member types
+	typedef BSTree<ft::pair<Key, T>, Compare, Alloc>		BST;
 	typedef Key												key_type;
 	typedef T												mapped_type;
 	typedef Compare											key_compare;
 	typedef Alloc											allocator_type;
-	typedef ft::pair<const key_type,mapped_type>			value_type;
+	typedef ft::pair<const key_type, mapped_type>			value_type;
 	typedef typename allocator_type::pointer				pointer;
 	typedef typename allocator_type::const_pointer			const_pointer;
 	typedef typename allocator_type::reference				reference;
 	typedef typename allocator_type::const_reference		const_reference;
+	typedef BIterator<*BST, value_type>						iterator;
+	typedef BIterator<const *BST, const value_type>			const_iterator;
 	
+	insert(BST* root, ft::pair<Key, T> hola)
+	{
+		
+	}
+	
+	map_iterate(BST* root)
+	{
+		
+	}
+
+	BST* Leftmost(BST* node)
+		{
+		    if (node == nullptr)
+		        return nullptr;
+		    while (node->left != nullptr)
+		        node = node->left;
+		    return node;
+		}
+		
+		// Start iterating from a root node
+		BST* First(BST* root)
+		{
+		    return Leftmost(root);
+		}
+		
+		// The iteration is current at node.  Return the next node
+		// in value order.
+		BST* Next(BST* node)
+		{
+		    // Make sure that the caller hasn't failed to stop.
+		    assert(node != nullptr);
+		
+		    // If we have a right subtree we must iterate over it,
+		    // starting at its leftmost (minimal) node.
+		
+		    if (node->right != nullptr)
+		        return Leftmost(node->right);
+		
+		    // Otherwise we must go up the tree
+		
+		    BST* parent = node->parent;
+		    if (parent == nullptr)
+		        return nullptr;
+		
+		    // A node comes immediately after its left subtree
+		
+		    if (node == parent->left)
+		        return parent;
+		
+		    // This must be the right subtree!
+		    assert(node == parent->right);
+		
+		    // In which case we need to go up again, looking for a node that is
+		    // its parent's left child.
+		
+		    while (parent != nullptr && node != parent->left)
+		    {
+		        node = parent;
+		        parent = node->parent;
+		    }
+		
+		    // We should be at a left child!
+		    assert(parent == nullptr || node == parent->left);
+		
+		    // And, as we know, a node comes immediately after its left subtree
+		
+		    return parent;
+		}
 };
 
 
